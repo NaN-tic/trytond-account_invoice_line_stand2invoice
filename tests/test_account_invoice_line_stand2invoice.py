9898e68dcdfddb63007c2dd0920349059adba1ae
resteve <resteve@zikzakmedia.com>
2015-12-01 10:57:21 +0100

    Tests

--- tests/test_account_invoice_line_stand2invoice.py
+++ tests/test_account_invoice_line_stand2invoice.py
@@ -1,22 +1,14 @@
-#!/usr/bin/env python
-# This file is part account_invoice_line_stand2invoice module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the account_invoice_line_stand2invoice module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class AccountInvoiceLineStand2invoiceTestCase(unittest.TestCase):
+class AccountInvoiceLineStand2invoiceTestCase(ModuleTestCase):
     'Test Account Invoice Line Stand2invoice module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module(
-                'account_invoice_line_stand2invoice')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'account_invoice_line_stand2invoice'
 
 
 def suite():
